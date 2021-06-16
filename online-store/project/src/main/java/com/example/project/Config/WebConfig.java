1b7714c7a8314ff53f0e9092b3e114f51f6666ee
ezdes <71791856+ezdes@users.noreply.github.com>
2021-06-16 14:28:12 +0600

    fix cors

--- online-store/project/src/main/java/com/example/project/Config/WebConfig.java
+++ online-store/project/src/main/java/com/example/project/Config/WebConfig.java
@@ -3,14 +3,15 @@ package com.example.project.Config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000");
     }
 }
