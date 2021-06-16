f4d8bfb43e16f465bea54231b2fec693b462423d
ezdes <ermekxt22@gmail.com>
2021-06-16 14:48:11 +0600

    add cors

--- online-store/project/src/main/java/com/example/project/Config/WebSecurityConfig.java
+++ online-store/project/src/main/java/com/example/project/Config/WebSecurityConfig.java
@@ -41,7 +41,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http.cors().disable().csrf().disable()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
