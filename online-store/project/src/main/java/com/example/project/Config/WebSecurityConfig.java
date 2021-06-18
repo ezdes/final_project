3f8513b5e211dff0abb683d8a48388d4a2a28f8f
ezdes <ermekxt22@gmail.com>
2021-06-18 20:02:14 +0600

    changed config

--- online-store/project/src/main/java/com/example/project/Config/WebSecurityConfig.java
+++ online-store/project/src/main/java/com/example/project/Config/WebSecurityConfig.java
@@ -78,10 +78,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
     }
-    
+
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**", "/configuration/**", "/swagger-ui.html/**", "/webjars/**");
+        web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**", "/configuration/**", "/swagger-ui.html", "/webjars/**");
 
     }
 
